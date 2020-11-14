9900a1d610d25acbd1419656822acbdbb26b3d02
Rohollah <rohollah.shirafkan@gmail.com>
2020-11-14 22:44:27 +0330

    Started Mobile First CSS

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,5 @@
 import '../styles/styles.css'
 
-if(module.hot){
-    module.hot.accept();
+if (module.hot) {
+  module.hot.accept()
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path')
 
 const postCSSPlugins = [
+
   require('postcss-import'),
+  require('postcss-mixins'),
   require('postcss-simple-vars'),
-  require('postcss-nested'),
-  require('autoprefixer')
-]
+  require('postcss-nested')//,
+  //require('autoprefixer')
+
+] 
 
 module.exports = {
   entry: './app/assets/scripts/App.js',
