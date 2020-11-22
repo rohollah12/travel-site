76cda73d22e4e7b5ae4826ac0a19054bd1927b73
Rohollah <rohollah.shirafkan@gmail.com>
2020-11-22 22:52:23 +0330

    Styling Finished

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,9 @@ const postCSSPlugins = [
   require('postcss-import'),
   require('postcss-mixins'),
   require('postcss-simple-vars'),
-  require('postcss-nested')//,
-  //require('autoprefixer')
+  require('postcss-nested'),
+  require('postcss-hexrgba'),
+  require('autoprefixer')
 
 ] 
 
