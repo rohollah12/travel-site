c1bad144b8635840325eaa82868cdae1a1da560b
Rohollah <rohollah.shirafkan@gmail.com>
2020-11-25 22:23:26 +0330

    Header End

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,14 @@
-import '../styles/styles.css'
+import '../styles/styles.css';
+import MobileMenu from "./modules/MobileMenu";
+
+
+let mobileMenu = new MobileMenu();
+
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
+
+
+
+
